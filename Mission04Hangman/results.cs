278e5169fb142a9bf219a526227354509dfde743
ccstephenson <ccstephenson@gmail.com>
2025-01-29 13:12:47 -0700

    Renamed the project to Mission04Hangman, created the results.cs file

---
+++ Mission04Hangman/results.cs
@@ -0,0 +1,13 @@
+namespace ConsoleApp1;
+
+public class GameResults
+{
+    /*The supporting class will:
+    • Receive the “board” array from the driver class
+        • Contain a method that prints the board based on the information passed to it
+    • Contain a method that receives the game board array as input and returns if there is a
+        winner and who it was*/
+    
+    
+    
+}
